71f947d842c7f4139551a155cdd1573832cb392d
andela-lozidu <lois.ozidu@andela.co>
2015-04-01 18:23:00 +0100

    Intial commit

---
+++ js/app.js
@@ -0,0 +1,119 @@
+var collisionCheck = 20;
+var rowImages = [83, 166, 249];
+var enemySpeedConst = 700;
+var Enemy = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.sprite = 'images/enemy-bug.png';
+    this.x = -100;
+    this.y = rowImages[Math.floor(Math.random() * 3)];
+    this.speed = Math.floor(100+ (Math.random() * enemySpeedConst));
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+   
+}
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x = this.x + (this.speed * dt);
+    if (this.x > 600) {
+        this.x = -100
+        this.y = this.y + 83;
+        this.speed = Math.floor(100 + (Math.random() * enemySpeedConst));
+        if (this.y > 249) {
+            this.y = 83;
+        }
+    }
+    if (player.y >= this.y - collisionCheck && player.y <= this.y + collisionCheck) {
+        if (player.x >= this.x - collisionCheck && player.x <= this.x + collisionCheck) {
+        player.reset();
+        
+        }
+    }
+}
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Now write your own player class
+var Player = function() {
+  this.x = 200;
+  this.y = 400;
+  this.image = 'images/char-boy.png';
+}
+
+Player.prototype.update = function() {
+    if (this.ctlKey === 'left' && this.x > 0) { 
+        this.x = this.x - 100; //player moves left
+    } 
+    else if (this.ctlKey === 'right' && this.x != 400) {
+        this.x = this.x + 100; //player moves right
+    } 
+    else if (this.ctlKey === 'up') {
+        this.y = this.y - 83; //player moves up
+    } 
+    else if (this.ctlKey === 'down' && this.y != 400) {
+        this.y = this.y + 83;
+    }
+    this.ctlKey = null;// no key is pressed
+    if (this.y < -20) {
+        this.reset();
+        //win.Play();
+    }
+}
+
+Player.prototype.render = function() {
+ ctx.drawImage(Resources.get(this.image), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(allowedKeys) {
+  this.ctlKey = allowedKeys;
+}
+
+Player.prototype.reset = function() {
+  player.x = 200;
+  player.y = 400;
+  
+  }
+
+
+// This class requires an update(), render() and
+// a handleInput() method.
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var enemyA = new Enemy();
+var enemyB = new Enemy();
+var enemyC = new Enemy();
+var enemyD = new Enemy();
+var allEnemies = [enemyA, enemyB, enemyC, enemyD];
+var player = new Player();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+
+
+
+
+
+
